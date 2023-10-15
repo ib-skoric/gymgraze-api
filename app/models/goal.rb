8304e75f75b5fe9b0a9619c0ca8cd5b9ec648694
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-10-15 17:42:08 +0100

    Add test + validations for presence of attributes on goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,6 @@
 class Goal < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
+  validates :steps, :kcal, :exercise, presence: true
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   belongs_to :user
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -2,12 +2,32 @@ require 'rails_helper'
 
 describe Goal, type: :model do
   describe "validations" do
-    it { should validate_presence_of :steps }
-    it { should validate_presence_of :kcal }
-    it { should validate_presence_of :exercise }
+    it "should validate presence of steps" do
+      no_steps_goal = Goal.new(steps: nil)
 
-    it { should validate_numericality_of(:steps).only_integer }
-    it { should validate_numericality_of(:kcal).only_integer }
-    it { should validate_numericality_of(:exercise).only_integer }
-  end
+      expect(no_steps_goal).to_not be_valid
+      expect(no_steps_goal.errors[:steps]).to include("can't be blank")
+    end
+
+    it "should validate presence of kcal goal" do
+      no_kcal_goal = Goal.new(kcal: nil)
+
+      expect(no_kcal_goal).to_not be_valid
+      expect(no_kcal_goal.errors[:kcal]).to include("can't be blank")
+    end
+
+    it "should validate presence of exercise goal" do
+      no_exercise_goal = Goal.new(exercise: nil)
+
+      expect(no_exercise_goal).to_not be_valid
+      expect(no_exercise_goal.errors[:exercise]).to include("can't be blank")
+    end
+
+    it "should validate presence of user" do
+      no_user_goal = Goal.new(user: nil)
+
+      expect(no_user_goal).to_not be_valid
+      expect(no_user_goal.errors[:user]).to include("must exist")
+    end
+    end
 end
