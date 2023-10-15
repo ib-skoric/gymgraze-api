a7c81237cddda66e0f7725b271792ce680f2b8ef
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-10-15 12:14:04 +0100

    Tidy up vars as constants

--- app/services/authentication_service.rb
+++ app/services/authentication_service.rb
@@ -1,10 +1,12 @@
 class AuthenticationService
+  HMAC_SECRET = "my$ecretK3y"
+  ALGORITHM_TYPE = 'HS256'
   def self.call
-    hmac_secret = "my$ecretK3y"
+
     # The secret must be a string. With OpenSSL 3.0/openssl gem `<3.0.1`, JWT::DecodeError will be raised if it isn't provided.
     payload = { "test" => "test123" }
 
-    token = JWT.encode payload, hmac_secret, 'HS256'
+    token = JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
 
     return token
   end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -4,9 +4,8 @@ require 'spec_helper'
 describe AuthenticationService do
   describe ".call" do
     it "should return an authentication token when called with valid credentials" do
-      hmac_secret = "my$ecretK3y"
       token = described_class.call
-      decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+      decoded_token = JWT.decode token, described_class::HMAC_SECRET, true, { algorithm: described_class::ALGORITHM_TYPE }
 
       expect(decoded_token).to eq([
                                     { "test" => "test123" },
