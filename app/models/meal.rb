78b8b0423a0fd5caee2867f7ac4073ff804d38a6
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-10-15 18:13:07 +0100

    Add title length check

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,7 @@
 class Meal < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
   validates :name, presence: true
+  validates :name, length: { minimum: 3 }
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   belongs_to :user
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -10,5 +10,12 @@ describe Meal, type: :model do
       expect(meal).to_not be_valid
       expect(meal.errors[:name]).to include("can't be blank")
     end
+
+    it "checks that the title is at least 3 characters" do
+      meal = Meal.create(name: "ab", user: user)
+
+      expect(meal).to_not be_valid
+      expect(meal.errors[:name]).to include("is too short (minimum is 3 characters)")
+    end
   end
 end
