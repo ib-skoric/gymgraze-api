78f82f595c8cb5246f8c771b41e23ffbaa07f9cb
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-10-15 10:43:04 +0100

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # ------------ MODEL VALIDATIONS ---------------- #
+  has_secure_password
+  validates :username, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 8 }
+
+  # ------------ MODEL ASSOCIATIONS --------------- #
 end
