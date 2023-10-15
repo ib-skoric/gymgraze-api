9a4e9b8e78920af39543d9952aed94f690e5e8fe
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-10-15 17:17:24 +0100

    Replace JWT to make test pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe UsersController do
+describe UserController do
 describe "GET to /users/:id endpoint" do
-    let (:user) { FactoryBot.create(:user) }
+    let (:user) { FactoryBot.create(:user, username: "testing", password: "testing2313543435", id: 4) }
 
-    it "returns the user" do
-      request.headers.merge!({'Authorization': "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w" })
-      get "show", params: { id: user.id }
+    it "returns the user whose token we passed in" do
+      request.headers.merge!({'Authorization': "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0fQ.Xv7yOoWo5RkZ6CJAfAkSKzeWA6ap9vjFIYr0FM2uq_k" })
+      get "index", params: { id: user.id }
 
       expect(response).to have_http_status(:ok)
+      expect(response.body).to eq({ user_id: user.id }.to_json)
     end
   end
 end
