b86df380203deb614f40e213579984bd6058fa3a
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-11-26 14:46:30 +0000

    update to use postgresql locally

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_15_170749) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "goals", force: :cascade do |t|
     t.integer "steps"
     t.integer "kcal"
     t.integer "exercise"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_15_170749) do
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_meals_on_user_id"
