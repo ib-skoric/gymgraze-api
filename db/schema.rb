44ab74bb33da4598bf90ffc72400549ca0a620dd
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-17 08:24:19 +0000

    remove entered_at since we can use created_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_081842) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_082345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "barcode"
-    t.datetime "entered_at"
     t.bigint "meal_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
