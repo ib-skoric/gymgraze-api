d482f6e6f5158e2156d2cf9ce7fe96abf8164a1e
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-18 21:35:28 +0000

    Remove all the seeds added by mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,3 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create(username: "superuser", password: "coolbeans")
-
-Goal.create(user_id: 1, calories: 2000, protein: 100, carbs: 100, fat: 100)
-
-Meal.create(user_id: 1, name: "Breakfast")
-Meal.create(user_id: 1, name: "Lunch")
-
-Food.create(name: "Egg", calories: 70, protein: 6, carbs: 0, fat: 5)
-Food.create(name: "Bacon", calories: 43, protein: 3, carbs: 0, fat: 3)
-Food.create(name: "Toast", calories: 75, protein: 3, carbs: 13, fat: 1)
-
-FoodDiaryEntry.create(date: "2021-09-01", user_id: 1)
