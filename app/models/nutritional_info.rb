10e6487efb1c5d03c5c70a86442a81edd2dff746
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-18 21:33:37 +0000

    Add food validation to macros

--- app/models/nutritional_info.rb
+++ app/models/nutritional_info.rb
@@ -1,6 +1,7 @@
 class NutritionalInfo < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
   validates :kcal, presence: true, numericality: { only_integer: true }, length: { minimum: 1 }
+  validates :food_id, presence: true, numericality: { only_integer: true }, allow_nil: false
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   belongs_to :food
