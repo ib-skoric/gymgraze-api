dce7813df40603036c7ca7071f4dae027fb7eae0
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-18 21:05:44 +0000

    add tests and validations for fooddiaryentry

--- app/models/food_diary_entry.rb
+++ app/models/food_diary_entry.rb
@@ -1,6 +1,7 @@
 class FoodDiaryEntry < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
-
+  # ensures date is present and unique for each user
+  validates :date, presence: true, uniqueness: { scope: :user_id }
 
   # ------------ MODEL ASSOCIATIONS ---------------- #
   belongs_to :user
