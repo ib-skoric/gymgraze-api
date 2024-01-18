5b7a3d88177f9ace87bbd52019c8ee066896a24c
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-18 21:32:53 +0000

    Add user validation to diary entry

--- app/models/food_diary_entry.rb
+++ app/models/food_diary_entry.rb
@@ -2,6 +2,7 @@ class FoodDiaryEntry < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
   # ensures date is present and unique for each user
   validates :date, presence: true, uniqueness: { scope: :user_id }
+  validates :user_id, numericality: { only_integer: true }, allow_nil: false
 
   # ------------ MODEL ASSOCIATIONS ---------------- #
   belongs_to :user
