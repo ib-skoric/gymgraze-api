821bd9eb07e6b7aeb8c865545e004f361ba6c352
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-18 21:34:08 +0000

    Add user validations to meal:

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,7 @@ class Meal < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
   validates :name, presence: true
   validates :name, length: { minimum: 3 }
+  validates :user_id, numericality: { only_integer: true }, allow_nil: false
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   belongs_to :user
