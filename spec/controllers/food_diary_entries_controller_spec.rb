ea3b24377b554676878d5322a862c4d1defbad4f
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-21 16:41:11 +0000

    Add more tests

--- spec/controllers/food_diary_entries_controller_spec.rb
+++ spec/controllers/food_diary_entries_controller_spec.rb
@@ -40,4 +40,53 @@ describe FoodDiaryEntryController do
       expect(body[0]["date"]).to eq(@food_entry.date.to_s)
     end
   end
+
+  describe "GET to /food_diary_entries/:id endpoint" do
+    it "should return a status of 200 OK" do
+      @food_entry = FactoryBot.create(:food_diary_entry, user_id: @user.id, date: Date.today)
+
+      request.headers.merge!(authentication_helper(@user))
+      get :show, params: { id: @food_entry.id }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should return a food diaries object serialized" do
+      @food_entry = FactoryBot.create(:food_diary_entry, user_id: @user.id, date: Date.today)
+
+      request.headers.merge!(authentication_helper(@user))
+      get :show, params: { id: @food_entry.id }
+
+      body = JSON.parse(response.body)
+      expect(body["id"]).to eq(@food_entry.id)
+      expect(body["date"]).to eq(@food_entry.date.to_s)
+    end
+  end
+
+  describe "POST to /food_diary_entries endpoint" do
+    it "should return a status of 201 Created" do
+      request.headers.merge!(authentication_helper(@user))
+      post :create, params: { food_diary_entry: { date: Date.today } }
+      expect(response).to have_http_status(:created)
+    end
+
+    it "should return a food diary entry object serialized" do
+      request.headers.merge!(authentication_helper(@user))
+      post :create, params: { food_diary_entry: { date: Date.today } }
+
+      body = JSON.parse(response.body)
+      expect(body["date"]).to eq(Date.today.to_s)
+    end
+
+    it "should return a status of 422 Unprocessable Entity when date is missing" do
+      request.headers.merge!(authentication_helper(@user))
+      post :create, params: { food_diary_entry: { date: nil } }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "should return a status of 422 Unprocessable Entity when date is invalid" do
+      request.headers.merge!(authentication_helper(@user))
+      post :create, params: { food_diary_entry: { date: "invalid date" } }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
