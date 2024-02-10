9d84dbc7fb2eef4ca01015feaa9074b0a9b2ed13
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-10 13:26:17 +0000

    Remove the need to authenticate user and add spec for creating users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,9 @@ class UserController < ApplicationController
   # used to get the token from the request header
   include ActionController::HttpAuthentication::Token
 
-  before_action :authenticate_user
-
   def index
+    before_action :authenticate_user
+
     render json: @user, serializer: UserSerializer
   end
 
