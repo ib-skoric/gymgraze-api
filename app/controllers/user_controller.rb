556631f7bfd2950443315c1a83cafe5abd2002f8
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-10 13:18:29 +0000

    Add method to be able to create a user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,5 +8,18 @@ class UserController < ApplicationController
     render json: @user, serializer: UserSerializer
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :age, weight)
+  end
 end
