9034000fcc42b178c5a1c70c58a4e9e925038edf
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-12 17:28:29 +0000

    Fix up bug with email confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def confirmation_email(user)
     @user = user
-    @confirmation_token = @user.confirmation_token
     mail(to: @user.email, subject: '🏋️‍♂️ GymGraze: Confirm your email')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
 
   # ------------ MODEL VALIDATIONS ---------------- #
   has_secure_password
-  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :age, presence: true
-  validates :weight, presence: true
-  validates :height, presence: true
+  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }, on: create
+  validates :password, presence: true, length: { minimum: 8 }, on: create
+  validates :name, presence: true, length: { minimum: 2 }, on: create
+  validates :age, presence: true, on: create
+  validates :weight, presence: true, on: create
+  validates :height, presence: true, on: create
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   has_one :goal
