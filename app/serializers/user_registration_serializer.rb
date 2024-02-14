88140b418e58b8c315d32e2eef63a96795731c74
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-14 16:37:28 +0000

    Add confirmed at to the serializer

--- app/serializers/user_registration_serializer.rb
+++ app/serializers/user_registration_serializer.rb
@@ -1,5 +1,5 @@
 class UserRegistrationSerializer < ActiveModel::Serializer
 
   # ------------ ATTRIBUTES ------------
-  attributes :id, :email, :name, :age, :weight, :height
+  attributes :id, :email, :name, :age, :weight, :height, :confirmed_at
 end
