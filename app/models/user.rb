e13ab8f23f3882e25bb116e905137394f02c7f29
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-18 12:49:27 +0000

    Fix up typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  before_create :generate_confirmation_token
+  after_create :generate_confirmation_token
 
   # ------------ MODEL VALIDATIONS ---------------- #
   has_secure_password
-  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }, on: create
-  validates :password, presence: true, length: { minimum: 8 }, on: create
-  validates :name, presence: true, length: { minimum: 2 }, on: create
-  validates :age, presence: true, on: create
-  validates :weight, presence: true, on: create
-  validates :height, presence: true, on: create
+  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }, on: :create
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :name, presence: true, length: { minimum: 2 }, on: :create
+  validates :age, presence: true, on: :create
+  validates :weight, presence: true, on: :create
+  validates :height, presence: true, on: :create
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   has_one :goal
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_124056) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_18_124626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,7 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_124056) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.datetime "confirmation_token_expires_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "food_diaries", "users"
