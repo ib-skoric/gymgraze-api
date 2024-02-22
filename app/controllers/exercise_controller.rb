a8d86da7f388adb28d74c1169131f819bcc5030c
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-22 21:34:39 +0000

    Create a method in the controller to create new exercise

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -1,2 +1,18 @@
 class ExerciseController < ApplicationController
+
+  def new
+    @exercise = Exercise.new(exercise_params)
+
+    if @exercise.save
+      render json: @exercise, status: :created
+    else
+      render json: @exercise.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :exercise_type)
+  end
 end
--- spec/requests/exercise_spec.rb
+++ spec/requests/exercise_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe "Exercises", type: :request do
 
       expect(response.body).to include("Squats")
     end
+
+    it "returns an error if the exercise is invalid" do
+      post "/exercises", params: { exercise: { name: nil, exercise_type: "strength" } }
+      expect(response).to have_http_status(422)
+
+      expect(response.body).to include("Name can't be blank")
+    end
+
+    it "returns an error if the exercise type is invalid" do
+      post "/exercises", params: { exercise: { name: "Squats", exercise_type: "something else" } }
+      expect(response).to have_http_status(422)
+
+      expect(response.body).to include("Exercise type is not included in the list")
+    end
   end
 end
