68e289857d33332f2c3e24943e2b17b061f791ce
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-25 12:38:46 +0000

    Create serializers and and include them in the controller logic

--- app/controllers/workout_diary_entries_controller.rb
+++ app/controllers/workout_diary_entries_controller.rb
@@ -4,12 +4,10 @@ class WorkoutDiaryEntriesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    diary_entry = WorkoutDiaryEntry.new(diary_entry_params)
-
-    diary_entry.user_id = @user.id
+    diary_entry = WorkoutDiaryEntry.new(diary_entry_params.merge(user_id: @user.id))
 
     if diary_entry.save
-      render json: diary_entry, status: :created
+      render json: diary_entry, status: :created, serializer: WorkoutDiaryEntrySerializer, include: ['workouts']
     else
       render json: diary_entry.errors, status: :unprocessable_entity
     end
@@ -17,7 +15,7 @@ class WorkoutDiaryEntriesController < ApplicationController
 
   def show
     diary_entry = WorkoutDiaryEntry.find(params[:id])
-    render json: diary_entry, status: :ok
+    render json: diary_entry, status: :ok, serializer: WorkoutDiaryEntrySerializer, include: ['workouts']
   end
 
   private
