8b4d619ea80111e7bb9de407a1636a9ec23630de
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-29 20:43:03 +0000

    Fix up sepcs

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,3 +1,10 @@
 class ExerciseSet < ApplicationRecord
+  # ------------ MODEL VALIDATIONS ---------------- #
+  validates :reps, presence: true, numericality: { greater_than: 0 }
+  validates :weight, presence: true
+  validates :exercise_id, presence: true
+
+
+  # ------------ MODEL ASSOCIATIONS ---------------- #
   belongs_to :exercise
 end
--- spec/models/exercise_set_spec.rb
+++ spec/models/exercise_set_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseSet, type: :model do
+
+  let(:exercise) { FactoryBot.create(:exercise) }
+
   it "should have a valid number of reps" do
     exercise_set = ExerciseSet.new(reps: 0)
     expect(exercise_set).to_not be_valid
@@ -12,7 +15,7 @@ RSpec.describe ExerciseSet, type: :model do
     exercise_set = ExerciseSet.new(weight: nil)
     expect(exercise_set).to_not be_valid
 
-    expect(exercise_set.errors[:weight]).to include("cannot be nil")
+    expect(exercise_set.errors[:weight]).to include("can't be blank")
   end
 
   it "should have a valid exercise_id" do
@@ -23,7 +26,7 @@ RSpec.describe ExerciseSet, type: :model do
   end
 
   it "should have reps, weight, and exercise_id present when created" do
-    exercise_set = ExerciseSet.new(reps: 5, weight: 100, exercise_id: 1)
+    exercise_set = ExerciseSet.new(reps: 5, weight: 100, exercise_id: exercise.id)
     expect(exercise_set).to be_valid
   end
 end
