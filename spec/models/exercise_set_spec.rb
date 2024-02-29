958d13c480a161edb64db44089cbf0f8d146f5a2
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-29 08:43:55 +0000

    Add basic spec for model

--- spec/models/exercise_set_spec.rb
+++ spec/models/exercise_set_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid number of reps" do
+    exercise_set = ExerciseSet.new(reps: 0)
+    expect(exercise_set).to_not be_valid
+
+    expect(exercise_set.errors[:reps]).to include("must be greater than 0")
+  end
+
+  it "should have a valid weight" do
+    exercise_set = ExerciseSet.new(weight: nil)
+    expect(exercise_set).to_not be_valid
+
+    expect(exercise_set.errors[:weight]).to include("cannot be nil")
+  end
+
+  it "should have a valid exercise_id" do
+    exercise_set = ExerciseSet.new(exercise_id: nil)
+    expect(exercise_set).to_not be_valid
+
+    expect(exercise_set.errors[:exercise_id]).to include("can't be blank")
+  end
+
+  it "should have reps, weight, and exercise_id present when created" do
+    exercise_set = ExerciseSet.new(reps: 5, weight: 100, exercise_id: 1)
+    expect(exercise_set).to be_valid
+  end
 end
