8387c127253df30b5336726b00eba481c6b9b873
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-02 10:44:34 +0000

    Add references

---
+++ db/migrate/20240302104331_add_references_for_exercise_and_workout_to_exercise_set.rb
@@ -0,0 +1,5 @@
+class AddReferencesForExerciseAndWorkoutToExerciseSet < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :exercise_sets, :workout, null: false, foreign_key: true
+  end
+end
