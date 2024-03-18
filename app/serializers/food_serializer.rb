03bb15859f4e0c877ecc0525850fbcc2323089a5
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-18 21:24:51 +0000

    Add amount to food and return calc values

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,8 +1,20 @@
 class FoodSerializer < ActiveModel::Serializer
-  attributes :id, :barcode, :name
+  attributes :id, :barcode, :name, :amount, :total_nutrition
 
   # ------------ RELATIONSHIPS ------------
   has_one :nutritional_info, serializer: NutritionalInfoSerializer
 
   has_one :meal, serializer: MealSerializer
+
+  def total_nutrition
+    {
+      kcal: ((object.nutritional_info.kcal / 100) * object.amount).to_i,
+      carbs: ((object.nutritional_info.carbs / 100) * object.amount).to_f,
+      protein: ((object.nutritional_info.protein / 100) * object.amount).to_f,
+      fat: ((object.nutritional_info.fat / 100) * object.amount).to_f,
+      salt: (10.0 / 100) * object.amount,
+      sugar: ((object.nutritional_info.sugar / 100) * object.amount).to_f,
+      fiber: ((object.nutritional_info.fiber / 100) * object.amount).to_f,
+    }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_18_122937) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_18_201325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_122937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "food_diary_entry_id"
+    t.integer "amount"
     t.index ["food_diary_entry_id"], name: "index_foods_on_food_diary_entry_id"
     t.index ["meal_id"], name: "index_foods_on_meal_id"
   end
