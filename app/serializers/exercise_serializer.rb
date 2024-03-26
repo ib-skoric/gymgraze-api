d5d594d03d54ff8ef94f3e77364eea3c8a7cba66
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-26 20:05:00 +0000

    Fix serializer and controller

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,5 +1,5 @@
 class ExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :exercise_type
+  attributes :id, :exercise_type_id
 
   has_many :exercise_sets, serializer: ExerciseSetSerializer
 end
