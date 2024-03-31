733731b9283a28437b48dd5f05a7e727d4711b04
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-31 12:43:38 +0100

    Fix where salt was being returned as string

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -6,13 +6,17 @@ class FoodSerializer < ActiveModel::Serializer
 
   has_one :meal, serializer: MealSerializer
 
+  def amount
+    object.amount.to_f
+  end
+
   def total_nutrition
     {
       kcal: ((object.nutritional_info.kcal / 100) * object.amount).to_i,
       carbs: ((object.nutritional_info.carbs / 100) * object.amount).to_f,
       protein: ((object.nutritional_info.protein / 100) * object.amount).to_f,
       fat: ((object.nutritional_info.fat / 100) * object.amount).to_f,
-      salt: (10.0 / 100) * object.amount,
+      salt: (10.0 / 100) * object.amount.to_f,
       sugar: ((object.nutritional_info.sugar / 100) * object.amount).to_f,
       fiber: ((object.nutritional_info.fiber / 100) * object.amount).to_f,
     }
