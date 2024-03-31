a33b31ed08c57a489d66d01499a5994830a0e561
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-31 13:42:53 +0100

    Serialize duration

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,5 +1,5 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :date, :workout_diary_entry_id
+  attributes :id, :date, :workout_diary_entry_id, :duration
 
   has_many :exercises, serializer: ExerciseSerializer
 end
