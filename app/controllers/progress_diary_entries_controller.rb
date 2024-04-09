1226897443d8e791436bdb07e92f36aa1912795b
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-09 20:35:51 +0100

    Add create and fetch by date methods

--- app/controllers/progress_diary_entries_controller.rb
+++ app/controllers/progress_diary_entries_controller.rb
@@ -1,2 +1,29 @@
 class ProgressDiaryEntriesController < ApplicationController
+  include ActionController::HttpAuthentication::Token
+
+  before_action :authenticate_user
+
+  def create
+    date = diary_entry_params[:date].to_date
+
+    diary_entry = ProgressDiaryEntry.new(date: date, user_id: @user.id)
+
+    if diary_entry.save
+      render json: diary_entry, status: :created, serializer: ProgressDiaryEntrySerializer, include: ['progress_diary_entries']
+    else
+      render json: diary_entry.errors, status: :unprocessable_entity
+    end
+  end
+
+  def fetch_by_date
+    diary_entry = ProgressDiaryEntry.find_by!(date: params[:date], user_id: @user.id)
+    render json: diary_entry, status: :ok, serializer: ProgressDiaryEntrySerializer
+  end
+
+  private
+
+  def diary_entry_params
+    params.require(:progress_diary_entry).permit(:date, :weight, :arm_measurement, :waist_measurement, :hip_measurements, :chest_measurement)
+  end
+
 end
--- app/serializers/progress_diary_entry_serializer.rb
+++ app/serializers/progress_diary_entry_serializer.rb
@@ -1,3 +1,3 @@
 class ProgressDiaryEntrySerializer < ActiveModel::Serializer
-  attributes :date, :weight, :arm_measurement, :waist_measurement, :hip_measurementg, :chest_measurement
+  attributes :date, :weight, :arm_measurement, :waist_measurement, :hip_measurements, :chest_measurement
 end
