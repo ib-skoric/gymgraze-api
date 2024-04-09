c6563a6bb339b6d4aeba7e7dd919b9aab2392a70
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-09 20:41:41 +0100

    Add uniquness scope to date

--- app/models/progress_diary_entry.rb
+++ app/models/progress_diary_entry.rb
@@ -1,7 +1,7 @@
 class ProgressDiaryEntry < ApplicationRecord
   # ------------ MODEL VALIDATIONS ---------------- #
   validates :weight, presence: true
-  validates :date, presence: true
+  validates :date, presence: true, uniqueness: { scope: :user_id }
 
   # ------------ MODEL ASSOCIATIONS --------------- #
   belongs_to :user, dependent: :destroy
