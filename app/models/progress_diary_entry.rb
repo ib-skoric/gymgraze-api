390b45cab0fc60a69bca1356d3b6eab0ef80fb4e
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-09 20:31:02 +0100

    Add validations and associations

--- app/models/progress_diary_entry.rb
+++ app/models/progress_diary_entry.rb
@@ -1,2 +1,8 @@
 class ProgressDiaryEntry < ApplicationRecord
+  # ------------ MODEL VALIDATIONS ---------------- #
+  validates :weight, presence: true
+  validates :date, presence: true
+
+  # ------------ MODEL ASSOCIATIONS --------------- #
+  belongs_to :user, dependent: :destroy
 end
