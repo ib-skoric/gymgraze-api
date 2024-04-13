dedcf9bae9ba98ace6ab2a4b3bca092235fd5a78
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-13 10:18:33 +0100

    Change value to float

--- app/serializers/progress_diary_entry_serializer.rb
+++ app/serializers/progress_diary_entry_serializer.rb
@@ -1,3 +1,23 @@
 class ProgressDiaryEntrySerializer < ActiveModel::Serializer
   attributes :id, :date, :weight, :arm_measurement, :waist_measurement, :hip_measurement, :chest_measurement
+
+  def weight
+    object.weight.to_f
+  end
+
+  def arm_measurement
+    object.arm_measurement.to_f
+  end
+
+  def waist_measurement
+    object.waist_measurement.to_f
+  end
+
+  def hip_measurement
+    object.hip_measurement.to_f
+  end
+
+  def chest_measurement
+    object.chest_measurement.to_f
+  end
 end
