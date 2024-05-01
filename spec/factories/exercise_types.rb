62150ba075db576fc2a8562d56d887ee454e14f2
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-01 20:43:46 +0100

    Fix up specs for exercise controller

--- spec/factories/exercise_types.rb
+++ spec/factories/exercise_types.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :exercise_type do
-    id { 1 }
+    id { rand(1..99999) }
     name { "MyString" }
     exercise_category { "strength" }
     user_id { FactoryBot.build(:jane_doe).id }
--- spec/factories/workouts.rb
+++ spec/factories/workouts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :workout do
-    id { 1 }
+    id { rand(1..99999) }
     date { "2024-02-25" }
     workout_diary_entry { FactoryBot.build(:workout_diary_entry) }
   end
