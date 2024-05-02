477f6e71ef7aaaaffdebc6c0b46aa9a5a5d9bea7
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:27:22 +0100

    Fix up template exercise spec

--- spec/models/template_exercise_spec.rb
+++ spec/models/template_exercise_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TemplateExercise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'belongs to a workout template' do
+    template_exercise = TemplateExercise.new
+    expect(template_exercise).to respond_to(:workout_template)
+  end
+
+  it 'belongs to an exercise type' do
+    template_exercise = TemplateExercise.new
+    expect(template_exercise).to respond_to(:exercise_type)
+  end
 end
