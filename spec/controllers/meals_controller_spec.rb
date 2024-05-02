266ea2abd5b0aa609982f4a534e4c4f3242407ba
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:44:00 +0100

    Add missing spec files for controller tests

---
+++ spec/controllers/meals_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'authentication_helper'
+
+describe MealsController, type: :controller do
+  # add something in here
+end
---
+++ spec/controllers/progress_diary_entries_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'authentication_helper'
+
+describe ProgressDiaryEntriesController, type: :controller do
+  # add something in here
+end
