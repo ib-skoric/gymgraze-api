a7d9fee31c8a9c00c5abdfc1ee3c713f287a7dd1
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 10:29:11 +0100

    Fix up exercise set model spec

--- spec/models/exercise_set_spec.rb
+++ spec/models/exercise_set_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ExerciseSet, type: :model do
 
-  let(:exercise) { FactoryBot.create(:exercise) }
+  before(:all) do
+    @user = FactoryBot.build(:jane_doe)
+    @workout_diary_entry = FactoryBot.create(:workout_diary_entry, user_id: @user.id)
+    @workout = FactoryBot.create(:workout, user_id: @user.id, workout_diary_entry_id: @workout_diary_entry.id)
+    @exercise = FactoryBot.create(:exercise, workout_id: @workout.id)
+  end
 
   it "should have a valid number of reps" do
-    exercise_set = ExerciseSet.new(reps: 0)
+    exercise_set = ExerciseSet.new(reps: nil)
     expect(exercise_set).to_not be_valid
 
-    expect(exercise_set.errors[:reps]).to include("must be greater than 0")
+    expect(exercise_set.errors[:reps]).to include("can't be blank")
   end
 
   it "should have a valid weight" do
@@ -18,15 +23,14 @@ RSpec.describe ExerciseSet, type: :model do
     expect(exercise_set.errors[:weight]).to include("can't be blank")
   end
 
-  it "should have a valid exercise_id" do
-    exercise_set = ExerciseSet.new(exercise_id: nil)
-    expect(exercise_set).to_not be_valid
+  it "should get created with a valid workout ID" do
+    exercise_set = ExerciseSet.new(workout_id: @workout.id, reps: 5, weight: 100, exercise_id: @exercise.id)
+    expect(exercise_set).to be_valid
 
-    expect(exercise_set.errors[:exercise_id]).to include("can't be blank")
   end
 
   it "should have reps, weight, and exercise_id present when created" do
-    exercise_set = ExerciseSet.new(reps: 5, weight: 100, exercise_id: exercise.id)
+    exercise_set = ExerciseSet.new(reps: 5, weight: 100, exercise_id: @exercise.id, workout_id: @workout.id)
     expect(exercise_set).to be_valid
   end
 end
