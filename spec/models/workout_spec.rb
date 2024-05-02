bbcceca4f845f35e96dd86512a8bae92d96f228b
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:35:49 +0100

    Add workout model specs

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validates presence of date and workout_diary_entry_id' do
+    it 'should validate presence of date' do
+      workout = Workout.new(date: nil, workout_diary_entry_id: 1)
+      expect(workout).to_not be_valid
+    end
+
+    it 'should validate presence of workout_diary_entry_id' do
+      workout = Workout.new(date: Date.today, workout_diary_entry_id: nil)
+      expect(workout).to_not be_valid
+    end
+
+    it 'should validate presence of date and workout_diary_entry_id' do
+      workout = Workout.new(date: nil, workout_diary_entry_id: nil)
+      expect(workout).to_not be_valid
+    end
+  end
+
+  it 'should be created as valid when all attributes are present' do
+    user = FactoryBot.build(:jane_doe)
+    workout = Workout.new(date: Date.today, workout_diary_entry_id: 1, user: user)
+    expect(workout).to be_valid
+  end
 end
