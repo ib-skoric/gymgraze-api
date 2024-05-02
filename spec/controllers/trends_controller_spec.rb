e585555278f829eab1ac40034765a092a296da48
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:57:12 +0100

    Add specs for trends

--- spec/controllers/trends_controller_spec.rb
+++ spec/controllers/trends_controller_spec.rb
@@ -3,5 +3,16 @@ require 'spec_helper'
 require 'authentication_helper'
 
 describe TrendsController, type: :controller do
-  # add something in here
+  before(:all) do
+    @user = FactoryBot.build(:jane_doe)
+  end
+
+  describe "GET /trends" do
+    it "fetches trends" do
+      request.headers.merge!(authentication_helper(@user))
+      get "index"
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
