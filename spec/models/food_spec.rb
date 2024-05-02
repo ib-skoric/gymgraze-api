54167da5cd94e7984d52274ca65f02a4f1ce16e4
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:24:12 +0100

    Fix up food model spec

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -22,16 +22,7 @@ RSpec.describe Food, type: :model do
       expect(no_meal_food).to_not be_valid
       expect(no_meal_food.errors[:meal]).to include("must exist")
     end
-
-    # though this will never be the case,
-    # since I'm doing TDD I may as well check for it
-    it "should validate presence of created_at" do
-      no_created_at_food = Food.new(created_at: nil)
-
-      expect(no_created_at_food).to_not be_valid
-      expect(no_created_at_food.errors[:created_at]).to include("can't be blank")
     end
-  end
 
   describe "it validates correct data types are used for each" do
     it "should return an error if barcode is not an integer" do
