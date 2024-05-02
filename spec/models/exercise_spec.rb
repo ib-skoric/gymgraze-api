a4506fd6fa00a98306d2ddd7cceffb6fa41757a0
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 10:51:48 +0100

    Fix up exercise specs

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,24 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
-  it "should validate presence of name" do
-    no_name_exercise = Exercise.new(name: nil)
+  it "should validate presence of user_id" do
+    no_name_exercise = Exercise.new(user_id: nil)
 
     expect(no_name_exercise).to_not be_valid
-    expect(no_name_exercise.errors[:name]).to include("can't be blank")
+    expect(no_name_exercise.errors[:user_id]).to include("can't be blank")
   end
 
   it "should validate present of type" do
     no_type_exercise = Exercise.new(exercise_type: nil)
 
     expect(no_type_exercise).to_not be_valid
-    expect(no_type_exercise.errors[:exercise_type]).to include("can't be blank")
-  end
-
-  it "should validate inclusion of type" do
-    invalid_type_exercise = Exercise.new(exercise_type: "something else")
-
-    expect(invalid_type_exercise).to_not be_valid
-    expect(invalid_type_exercise.errors[:exercise_type]).to include("is not included in the list")
+    expect(no_type_exercise.errors[:exercise_type]).to include("must exist")
   end
 end
