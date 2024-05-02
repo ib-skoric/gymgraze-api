4ee1931acbdd7e177250b381a0c337374a154916
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:38:07 +0100

    Add specs for workout template

--- spec/models/workout_template_spec.rb
+++ spec/models/workout_template_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe WorkoutTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have many template exercises' do
+    template = WorkoutTemplate.reflect_on_association(:template_exercises)
+    expect(template.macro).to eq(:has_many)
+  end
+
+  it 'should have many exercises' do
+    template = WorkoutTemplate.reflect_on_association(:exercise_types)
+    expect(template.macro).to eq(:has_many)
+  end
+
+  it 'should belong to a user' do
+    template = WorkoutTemplate.reflect_on_association(:user)
+    expect(template.macro).to eq(:belongs_to)
+  end
 end
