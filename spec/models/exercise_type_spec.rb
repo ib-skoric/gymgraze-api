51c2aad9a71ebd0d4dbb910bacbe8fc267a50e22
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-02 21:21:15 +0100

    Add specs for exercise type model

--- spec/models/exercise_type_spec.rb
+++ spec/models/exercise_type_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presence of name' do
+    exercise_type = ExerciseType.new(name: nil)
+    expect(exercise_type).to be_invalid
+  end
+
+  it 'validates presence of exercise_category' do
+    exercise_type = ExerciseType.new(exercise_category: nil)
+    expect(exercise_type).to be_invalid
+  end
+
+  it 'validates inclusion of exercise_category' do
+    exercise_type = ExerciseType.new(exercise_category: 'yoga')
+    expect(exercise_type).to be_invalid
+  end
+
+  it 'is created when all attributes are valid' do
+    exercise_type = ExerciseType.new(name: 'Squats', exercise_category: 'strength')
+    expect(exercise_type).to be_valid
+  end
+
+  it 'has many exercises' do
+    exercise_type = ExerciseType.reflect_on_association(:exercises)
+    expect(exercise_type.macro).to eq(:has_many)
+  end
 end
