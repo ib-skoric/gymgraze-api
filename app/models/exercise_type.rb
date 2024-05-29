d8e7b61f553e3777602222fd1d2a1b2941208882
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-29 20:28:10 +0100

    Add a thing to delete exercises

--- app/models/exercise_type.rb
+++ app/models/exercise_type.rb
@@ -4,7 +4,7 @@ class ExerciseType < ApplicationRecord
   validates :exercise_category, presence: true, inclusion: { in: %w(cardio strength) }
 
   # ------------ MODEL ASSOCIATIONS ---------------- #
-  has_many :exercises
+  has_many :exercises, dependent: :destroy
   has_many :template_exercises, dependent: :destroy
   has_many :workout_templates, through: :template_exercises
 end
